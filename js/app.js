e2e515769f50cbe9e2f27376616d09a67b2cc250
Edward Popescu <edwardp383@gmail.com>
2019-04-03 00:33:58 -0500

    got tamagotchi to morph at age 2

--- js/app.js
+++ js/app.js
@@ -2,26 +2,26 @@
 class TamagatchiChar{
 	constructor(name) {
 		this.name = name;
-		this.hunger = 10;
-		this.sleep = 10;
+		this.fullness = 10;
+		this.rest = 10;
 		this.fun = 10;
 		this.age = 0;
 	}
 	getHungry() {
 		if(game.time % 4 === 0){
-			this.hunger--;
+			this.fullness--;
 		}
 	}
 	feed() {
-		this.hunger += 2
+		this.fullness += 2
 	}
 	getTired() {
 		if (game.time % 10 === 0) {
-			this.sleep--;
+			this.rest--;
 		}
 	}
-	goToSleep() {
-		this.sleep = 10
+	goTorest() {
+		this.rest = 10
 	}
 
 	getBored() {
@@ -51,6 +51,9 @@ const game = {
 		this.tama = character;
 	},
 	grow(){
+		if (game.tama.age == 2) {
+			$('#tamagotchi > img').replaceWith('<img src="images/tamagotchi teen.gif">')
+		}
 
 	},
 	die(){
@@ -62,6 +65,8 @@ const game = {
 			this.tama.getTired()
 			this.tama.getBored()
 			this.tama.getOlder()
+			this.grow()
+			this.die()
 			console.log(this.tama)
 		}, 1000)
 
@@ -73,7 +78,8 @@ const game = {
 $('form').on('submit', (e) => {
 	e.preventDefault();
 	game.hatch($('#input-name').val())
-	$('form').replaceWith(`<h2>${game.tama.name}<h2>`)
+	$('form').replaceWith(`<h2>${game.tama.name}<h2>`);
+	$('#tamagotchi > img').replaceWith('<img src="images/tamagotchi baby.gif">');
 	game.startTime();
 });
 
