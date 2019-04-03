f6d16255e7525c2947d1f2d918b46a003ded300f
Edward Popescu <edwardp383@gmail.com>
2019-04-02 23:16:25 -0500

    got the stats to change over time plus additions to class

--- js/app.js
+++ js/app.js
@@ -1,47 +1,77 @@
 // 1 tamagatchi year is 24hrs
 class TamagatchiChar{
-	constructor(name){
-		this.name = name
-		this.hunger = 10
-		this.sleep = 10
-		this.fun = 10
-		this.age = 0
+	constructor(name) {
+		this.name = name;
+		this.hunger = 10;
+		this.sleep = 10;
+		this.fun = 10;
+		this.age = 0;
 	}
-	getHungry(){
-		this.hunger--
+	getHungry() {
+		if(game.time % 4 === 0){
+			this.hunger--;
+		}
 	}
-	feed(){
+	feed() {
 		this.hunger += 2
 	}
-	getTired(){
-		this.sleep--
+	getTired() {
+		if (game.time % 10 === 0) {
+			this.sleep--;
+		}
 	}
-	goToSleep(){
+	goToSleep() {
 		this.sleep = 10
 	}
 
-	getBored(){
-		this.fun--
+	getBored() {
+		if(game.time % 3 === 0){
+			this.fun--;
+		}
 	}
-	play(){
+	play() {
 		this.fun += 2
 	}
-	
-}
-const tama = new TamagatchiChar("boop");
-console.log(tama)
+	getOlder(){
+		if(game.time % 20 === 0){
+			this.age++;
+		}
+	}
+};
+
+
 const game = {
 	time: 0,
 	gameStarted: false,
 	tama: undefined,
-	hatch(name){
-		
+	hatch(name) {
+		const character = new TamagatchiChar(name);
+		this.tama = character;
 	},
 	grow(){
 
 	},
-	startTime(){
+	die(){
+		
+	},
+	startTime() { setInterval(() => {
+			this.time++
+			this.tama.getHungry()
+			this.tama.getTired()
+			this.tama.getBored()
+			this.tama.getOlder()
+			console.log(this.tama)
+		}, 1000)
 
-	}
+	},
 
-}
+};
+
+game.hatch("boop")
+game.startTime()
+
+
+
+
+
+
