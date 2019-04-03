bd4a4e8a590d0c035b9b3300723b493ef9eb7f73
Edward Popescu <edwardp383@gmail.com>
2019-04-02 19:03:02 -0500

    made the Tamagotchi console with html/css

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// 1 tamagatchi year is 24hrs
+class TamagatchiChar{
+	constructor(name){
+		this.name = name
+		this.hunger = 10
+		this.sleep = 10
+		this.fun = 10
+		this.age = 0
+	}
+	getHungry(){
+
+	}
+	getTired(){
+
+	}
+	getBored
+}
+
+
+
+const game = {
+	time: 0,
+	gameStarted: false,
+
+}
